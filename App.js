69375897c03738633bc1e44e4f5d500ac5ca4f7c
Jonathan Henrique Galdino <jhenrique87@yahoo.com.br>
2020-07-12 18:00:13 -0300

    Projeto biometria simples

--- App.js
+++ App.js
@@ -1,12 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as LocalAuthentication from 'expo-local-authentication';
 
 export default function App() {
+  const [isValidAuth, setValidAuth] = useState(False);
+  const [errorMessage,setErrorMessage] = useState('');
+
+  useEffect() >= {
+    checkDeviceForHardware();
+    
+  
+
+  },[];
+
+  const checkDeviceForHardware = async () >= {
+    let compatible = await LocalAuthentication.hasHardwareAsync();
+    if (!compatible){
+      setErrorMessage('Dispositivo incompativel');
+    }else{
+      checkBiometrics();
+    }
+    
+  }
+const checkBiometrics = async () >= {
+  let biometricRecords = LocalAuthentication.isEnrolledAsync();
+  if(!biometricRecords){
+    setErrorMessage('Sem digitais cadastradas');
+
+  }else{
+    verifybiometric();
+  }
+}
+const verifybiometric = async() >= {
+let result = await LocalAuthentication.authenticateAsync();
+
+if(result.sucess){
+  setValidAuth(true);
+  alert('Digital Reconhecida');
+}else{
+  setValidAuth(false);
+  setErrorMessage('Digital Invalida');
+}
+
+  // se o aparelho é compativel com impressão
+  // se existem digitais cadastradas
+  // se a digital é válida
+  
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {
+        isValidAuth ?
+        <Text>Digital Valida</Text>
+        :
+        <Text>Digital Invalida</Text>
+      } 
+      <Text>{errorMessage}</Text>  
     </View>
   );
 }
